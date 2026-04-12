da6905ab263656f7ab1eb1ccae6a0457020d8c9c
Marley Doughty <marleydoughty@Marleys-MacBook-Air.local>
2026-04-12 16:58:14 -0700

    init: file restructure and updated readme

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -14,8 +14,7 @@ const db = new pg.Pool({
 
 const app = express();
 
-const reactStaticDir = new URL('../../frontend/client/dist', import.meta.url)
-  .pathname;
+const reactStaticDir = new URL('../../frontend/dist', import.meta.url).pathname;
 const uploadsStaticDir = new URL('public', import.meta.url).pathname;
 
 app.use(express.static(reactStaticDir));
--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import faviconUrl from '../assets/womens-day.png';
 import './index.scss';
+
+const favicon = document.createElement('link');
+favicon.rel = 'icon';
+favicon.type = 'image/png';
+favicon.href = faviconUrl;
+document.head.appendChild(favicon);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
